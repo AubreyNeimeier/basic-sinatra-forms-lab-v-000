23d8cc4e160540e1e7f3c2fa8593562357588b13
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-19 17:13:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ class App < Sinatra::Base
 
   get '/newteam' do
     erb :newteam
+    #the newteam erb file is a form. The method for the form is post and the action is /team. This is because the info needs to be POSTED. Where? Not on this HTML page, but on the /Team html page/view.
   end
 
 
   post '/team' do
-
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
